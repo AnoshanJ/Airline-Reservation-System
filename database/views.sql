eef8018f9ed143f3b14f443b9a87e97325db5982
Anoshan Jayahanthan <anoshan265@gmail.com>
2022-12-30 15:03:43 +0530

    updated database files

---
+++ database/views.sql
@@ -0,0 +1,11 @@
+psql -U postgres -h localhost : to connect to database
+postgres=# CREATE ROLE admin WITH LOGIN PASSWORD '1234';
+GRANT ALL ON ALL TABLES IN SCHEMA public TO admin;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO admin;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO admin;
+
+-- Amrithsagar Search View
+CREATE OR REPLACE VIEW search_table AS 
+SELECT flight_id, origin, departure_date, departure_time, destination, arrival_date, arrival_time 
+FROM flight_schedule
+JOIN route ON flight_schedule.route_id = route.route_id;
